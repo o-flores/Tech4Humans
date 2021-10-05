621299de6d22414bf1b14135656df1372d7e80d1
Orlando Flores <orlando_flores@id.uff.br>
2021-10-05 10:45:50 -0300

    feat: creates input

--- front-end/src/components/WeatherSearch/index.jsx
+++ front-end/src/components/WeatherSearch/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import { FaSearch } from 'react-icons/fa';
 
 function WeatherSearch() {
+  const [isInputActive, setIsInputActive] = useState(false);
+  const [query, setQuery] = useState('');
   return (
     <div>
-      oi
+      <FaSearch onClick={() => setIsInputActive(!isInputActive)} />
+      {isInputActive && (
+        <input
+          type="text"
+          placeholder="Busque pelo nome da cidade"
+          onChange={({ target }) => setQuery(target.value)}
+          value={query}
+        />
+      )}
     </div>
   );
 }
