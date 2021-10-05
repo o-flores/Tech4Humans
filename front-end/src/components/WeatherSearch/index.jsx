4d598a4cfdee8cbbd00c7509e507a975f2bea7bb
Orlando Flores <orlando_flores@id.uff.br>
2021-10-05 12:20:57 -0300

    feat: fetch weather info by form input

--- front-end/src/components/WeatherSearch/index.jsx
+++ front-end/src/components/WeatherSearch/index.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from 'react';
 import './style.css';
 import { FaSearch } from 'react-icons/fa';
+import api from '../../services/api';
 
 function WeatherSearch() {
   const [isInputActive, setIsInputActive] = useState(false);
   const [query, setQuery] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const city = await api.searchCityByName(query);
+    console.log(city);
+  }
   return (
     <div>
       <FaSearch onClick={() => setIsInputActive(!isInputActive)} />
       {isInputActive && (
-        <input
-          type="text"
-          placeholder="Busque pelo nome da cidade"
-          onChange={({ target }) => setQuery(target.value)}
-          value={query}
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Busque pelo nome da cidade"
+            onChange={({ target }) => setQuery(target.value)}
+            value={query}
+          />
+          <button
+            type="submit"
+          >
+            Procurar
+          </button>
+        </form>
       )}
     </div>
   );
