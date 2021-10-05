1c15d540b8199d9d8ccf4a1920804b61d0074134
Orlando Flores <orlando_flores@id.uff.br>
2021-10-05 10:03:56 -0300

    feat: creates post route

--- Back-End/index.js
+++ Back-End/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const controller = require('./controller');
 
 app.use(bodyParser.json());
+
+app.post('/weather', controller.postWeather);
 
 app.listen(port);
