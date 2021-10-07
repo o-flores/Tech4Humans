1503badd64a839726e08d7997d831e519a1aa4d1
Orlando Flores <orlando_flores@id.uff.br>
2021-10-07 11:54:32 -0300

    fix: requisition params

--- Back-End/controller/index.js
+++ Back-End/controller/index.js
@@ -1,8 +1,8 @@
 const service = require('../services');
 
 const postWeather = async (req, res, next) => {
-  const { q: city } = req.query;
-  const response = await service.postWeather({ city });
+  const { unit, city } = req.body;
+  const response = await service.postWeather({ city, unit });
 
   if (response.error) return next(response);
 
