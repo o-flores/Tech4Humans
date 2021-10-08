6eb4a11cbe473fbe95aff9e85d003babccfe3513
Orlando Flores <orlando_flores@id.uff.br>
2021-10-08 15:54:10 -0300

    feat: add error handling in error middleware

--- Back-End/middlewares/error.js
+++ Back-End/middlewares/error.js
@@ -1,6 +1,10 @@
-const errorMiddleware = (error, req, res) => {
-  const { code, message } = error;
-  return res.status(code).json({ err: { message } });
+/* eslint-disable no-unused-vars */
+const errorMiddleware = (err, _req, res, _next) => {
+  const { code, message } = err;
+
+  if (typeof code !== 'number') return res.status(500).json({ error: { message } });
+
+  return res.status(code).json({ error: { message } });
 };
 
 module.exports = errorMiddleware;
