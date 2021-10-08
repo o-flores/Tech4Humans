af4102a8a1d0fa65fec4c4379c1139190d34458e
Orlando Flores <orlando_flores@id.uff.br>
2021-10-08 16:09:49 -0300

    feat: creates controller

--- Back-End/controller/index.js
+++ Back-End/controller/index.js
@@ -4,13 +4,24 @@ const postCity = async (req, res, next) => {
   const { unit, city } = req.body;
   const response = await service.postCity({ city, unit });
 
-  if (response.error) return next(response);
+  if (response.code) return next(response);
 
   if (response.created) return res.status(201).json(response);
 
   return res.status(200).json(response);
 };
 
+const getLastSearchs = async (req, res, next) => {
+  const { limit } = req.query;
+
+  const response = await service.getLastSearchs(limit);
+
+  if (response.error) return next(response);
+
+  return res.status(200).json(response);
+};
+
 module.exports = {
   postCity,
+  getLastSearchs,
 };
