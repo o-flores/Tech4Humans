07c87a76d24911f938148ea7e9ac7febcdb6150b
Orlando Flores <orlando_flores@id.uff.br>
2021-10-08 16:30:49 -0300

    feat: add SearchHistory to App and add css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './App.css';
+import SearchHistory from './components/SearchHistory';
 import WeatherSearch from './components/WeatherSearch/index';
 
 function App() {
   return (
     <div className="App">
       <WeatherSearch />
+      <SearchHistory />
     </div>
   );
 }
